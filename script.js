7abd70fbe394ca97dc1cc22b1f145e4e606c6301
Praveen Katikireddy <praveenk9417@gmail.com>
2023-04-10 14:38:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function fibonacci(num) {
 // your code here
 	if(num==1) return 0;
-	if(num==2) return 1;
-	return fibonacci(num-1)+fibonacci(num-2);
+	let a = 0, b =1;
+	for(let i=3; i<=num; i++) {
+		let temp = b;
+		b = a+b;
+		a = temp;
+	}
+	return b;
 }
 
 module.exports = fibonacci;
